1d25db30f4c0695d785ea8e9f90d1963213c5d63
mansii1 <mansimangukiya0@gmail.com>
2023-01-12 16:55:12 +0530

    commit

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-const mongoose = require('mongoose')
-const express = require("express")
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const route = require('./src/routes/index');
-const bodyParser=require("body-parser")
-app.use(bodyParser.json())
+const route = require("./src/routes/index");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const path = require("path");
 
-const cookieparser=require("cookie-parser")
-app.use(cookieparser())
-const cors=require("cors");
+const cookieparser = require("cookie-parser");
+app.use(cookieparser());
+const cors = require("cors");
 
 app.use(
   cors({
-    credentials: true, 
+    credentials: true,
   })
-  );
-app.use("/", route)
+);
+app.use("/", route);
 
-mongoose.connect('mongodb://localhost:27017/crud')
+app.use(
+  express.static(
+    path.join(__dirname, "task-management-system-frontend", "build")
+  )
+);
+app.get("/login", function (req, res) {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "task-management-system-frontend",
+      "build",
+      "index.html"
+    )
+  );
+});
+mongoose
+  .connect("mongodb://localhost:27017/crud")
   .then(() => {
-    console.log("connection done")
-  }).catch((error) => {
-    console.log("connection error", error)
+    console.log("connection done");
+  })
+  .catch((error) => {
+    console.log("connection error", error);
   });
-  
-  
-app.listen(9000, () => console.log("port on : 9000"))
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`port on : ${PORT}`));
