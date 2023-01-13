fd693dfbf5c25df3142b4c74f51cddd7b2cb96e6
mansii1 <mansimangukiya0@gmail.com>
2023-01-13 15:46:46 +0530

    uncomment route

--- src/model/taskModel.js
+++ src/model/taskModel.js
@@ -1,18 +1,18 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const taskSchema = new mongoose.Schema({
-//   task_name: String,
-//   task_description: String,
-//   created_at: { type: Date, default: Date.now, required: true },
-//   created_by: mongoose.Schema.Types.ObjectId,
-//   duration: Number,
-//   assign_to: mongoose.Schema.Types.ObjectId,
-//   start_task: Date,
-//   end_task: Date,
-//   total_time: Number,
-//   assignId: mongoose.Schema.Types.ObjectId,
-//   task_status: String,
-// });
+const taskSchema = new mongoose.Schema({
+  task_name: String,
+  task_description: String,
+  created_at: { type: Date, default: Date.now, required: true },
+  created_by: mongoose.Schema.Types.ObjectId,
+  duration: Number,
+  assign_to: mongoose.Schema.Types.ObjectId,
+  start_task: Date,
+  end_task: Date,
+  total_time: Number,
+  assignId: mongoose.Schema.Types.ObjectId,
+  task_status: String,
+});
 
-// const taskList = mongoose.model("task", taskSchema);
-// module.exports = taskList;
+const taskList = mongoose.model("task", taskSchema);
+module.exports = taskList;
