d36d86d326d73a09bdfd67e8cc9a236f868bdc25
mansii1 <mansimangukiya0@gmail.com>
2023-01-13 16:18:30 +0530

    task model uncommited

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,9 @@
 const sendResponse = (req, res, data, status = 200) => {
-    res.status(status).send({ data, refreshToken: req.refreshToken ? req.refreshToken : undefined })
-
-
-}
-module.exports = { sendResponse }
+  res
+    .status(status)
+    .send({
+      data,
+      refreshToken: req.refreshToken ? req.refreshToken : undefined,
+    });
+};
+module.exports = { sendResponse };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const loginRoute = require("./loginRoute");
 const userRoute = require("./userRoute");
-// const taskRoute = require("./taskRoute");
+const taskRoute = require("./taskRoute");
 const dashboardRoute = require("./dashboardRoute");
 const route = express.Router();
 
 route.use("/", loginRoute);
 route.use("/", userRoute);
-// route.use("/", taskRoute);
+route.use("/", taskRoute);
 route.use("/", dashboardRoute);
 
 module.exports = route;
