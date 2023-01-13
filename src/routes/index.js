647f486735b6b47f9c3c36908d24176910be6548
mansii1 <mansimangukiya0@gmail.com>
2023-01-13 15:19:47 +0530

    comment all

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const loginRoute = require("./loginRoute");
 const userRoute = require("./userRoute");
 // const taskRoute = require("./taskRoute");
-const dashboardRoute = require("./dashboardRoute");
+// const dashboardRoute = require("./dashboardRoute");
 const route = express.Router();
 
 route.use("/", loginRoute);
 route.use("/", userRoute);
 // route.use("/", taskRoute);
-route.use("/", dashboardRoute);
+// route.use("/", dashboardRoute);
 
 module.exports = route;
