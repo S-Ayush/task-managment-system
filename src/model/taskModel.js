9d5691642767aabd34c797ab91d39d6158e4de2a
mansii1 <mansimangukiya0@gmail.com>
2023-01-13 16:38:19 +0530

    model changes

--- src/model/taskModel.js
+++ src/model/taskModel.js
@@ -14,5 +14,19 @@ const taskData = new mongoose.Schema({
   task_status: String,
 });
 
-const taskList = mongoose.model("task", taskData);
-module.exports = taskList;
+const tasks = new mongoose.Schema({
+  task_name: String,
+  task_description: String,
+  created_at: { type: Date, default: Date.now, required: true },
+  created_by: mongoose.Schema.Types.ObjectId,
+  duration: Number,
+  assign_to: mongoose.Schema.Types.ObjectId,
+  start_task: { type: Date },
+  end_task: { type: Date },
+  total_time: Number,
+  assignId: mongoose.Schema.Types.ObjectId,
+  task_status: String,
+});
+
+const data = mongoose.model("task", tasks);
+module.exports = data;
